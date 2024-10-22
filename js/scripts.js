797ff646f66ae17fbebbd71b76a4519c1ce7138f
Digital-dementia <merdix515@gmail.com>
2024-10-22 17:23:51 +0200

    updt

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+// - Crea una función que reciba un array de 5 números, imprime por consola una posición aleatoria del array.
+
+
+const array1 = [1,2,3,4,5];
+  const  aleatory = Math.floor(Math.random()* array1.length)
+    console.log (array1[aleatory]);
+
+
+
+// Crea una función que reciba un array con 3 números. La función deberá imprimir por consola. "La suma de todos los números es:[suma]"- "La media de todos los números es:[media]"- "El mayor es [mayor] y el menor es [menor]"
+
+    const array2 = [2,10,12];
+        const sume = [0+1+2];
+        const average = [0+1+2] /3;
+            console.log ('la suma de todos los números es: ' [sume]);
+
